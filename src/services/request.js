bbea02631d4561b953d773f78c92bbfad034006f
lbgomes <muriloluiz2003@gmail.com>
2022-09-29 22:58:05 -0300

    feat: starting project

---
+++ src/services/request.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export const signIn = async (data) => {
+  console.log(data)
+    const createContentDataResponse = await axios.get('http://localhost:3000/api/entrar', data)
+
+    return createContentDataResponse
+  }
+
+  export const SignUp = async (data) => {
+  
+    const createContentDataResponse = await axios.post('http://localhost:3000/api/cadastrar', data)
+    
+    return createContentDataResponse
+  }
